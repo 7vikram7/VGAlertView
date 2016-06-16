60b57e11fe5a548fd596ae7cbb697cca23df675e
Vikram Gore <vikram.gore@mobisoftinfotech.com>
2016-06-16 18:39:05 +0530

    Updated project structure

---
+++ VGAlertView/ThirdParty/VGAlertView/VGAlertView.swift
@@ -0,0 +1,35 @@
+//
+//  VGAlertView.swift
+//  VGAlertView
+//
+//  Created by Vikram Gore on 16/06/16.
+//  Copyright © 2016 7vikram7. All rights reserved.
+//
+
+import UIKit
+
+class VGAlertView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
