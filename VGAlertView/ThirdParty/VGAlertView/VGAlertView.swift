ea932833272cb2d9f3f17442b8a5695a4a4453bd
7vikram7 <vikramgore2007@gmail.com>
2016-06-19 22:15:13 +0530

    Removed needless code

--- VGAlertView/ThirdParty/VGAlertView/VGAlertView.swift
+++ VGAlertView/ThirdParty/VGAlertView/VGAlertView.swift
@@ -8,24 +8,14 @@
 
 import UIKit
 
-class VGAlertView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class VGAlertView: UIViewController {
 
-    let tableview = UITableView()
-    
     
     //MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        tableview.delegate = self
-        tableview.dataSource = self
-        tableview.translatesAutoresizingMaskIntoConstraints = false
-        
-//        tableview.addConstraint(NSLayoutConstraint(item: tableview, attribute:.Top, relatedBy: .Equal, toItem: self, attribute:.Top, multiplier: 1, constant: 0))
-//        tableview.addConstraint(NSLayoutConstraint(item: tableview, attribute:.b, relatedBy: .Equal, toItem: self, attribute:.Top, multiplier: 1, constant: 0))
-//        tableview.addConstraint(NSLayoutConstraint(item: tableview, attribute:.Top, relatedBy: .Equal, toItem: self, attribute:.Top, multiplier: 1, constant: 0))
-//        tableview.addConstraint(NSLayoutConstraint(item: tableview, attribute:.Top, relatedBy: .Equal, toItem: self, attribute:.Top, multiplier: 1, constant: 0))
         
     }
 
@@ -34,43 +24,7 @@ class VGAlertView: UIViewController, UITableViewDelegate, UITableViewDataSource 
     func showAlertOnViewController(parentViewController vc:UIViewController, title:String, message:String ) {
      
         
-        
     }
-    
-    
-    //MARK: UITableView Delegate Methods
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-//        NSArray *visibleIndexPaths = [tableView indexPathsForVisibleRows];
-//        
-//        NSMutableArray *visibleCellsArray = [[NSMutableArray alloc] init];
-//        
-//            //  You now have visible cells in visibleCellsArray
-//            [visibleCellsArray addObject:[tableView cellForRowAtIndexPath:currentPath]];
-//        }
-        
-        let visibleIndexPaths = tableview.indexPathsForVisibleRows
-        
-        var visibleCellsArray: [UITableViewCell] = []
-        
-        for currentIndextPath in visibleIndexPaths! {
-           //  You now have visible cells in visibleCellsArray
-            visibleCellsArray.append(tableview.cellForRowAtIndexPath(currentIndextPath)!)
-        }
-        
-        let cell = UITableViewCell()
-        return cell
-    }
-
-//    - (void)showAttributedEdit:(UIViewController *)vc headerTitle:(NSString *)headerTitle headerSubTitle:(NSMutableAttributedString *)headerSubTitle title:(NSMutableAttributedString *)title subTitle:(NSMutableAttributedString *)subTitle closeButtonTitle:(NSString *)closeButtonTitle duration:(NSTimeInterval)duration;
-
-
-
     
     /*
     // MARK: - Navigation
